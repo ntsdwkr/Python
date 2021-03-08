0d4792e8f5d1aadee1e5603bf450e89fbed76e1f
Nitish Diwakar <nitish7470918759@gmail.com>
2021-03-09 00:06:55 +0530

    Update linearSearch.py

--- linearSearch.py
+++ linearSearch.py
@@ -1,8 +1,9 @@
+# Linear Search
 def linearSearch(a,num):
     for i in range(len(a)):
         if num==a[i]:
             return i
     return -1
 
-a=[int(x) for x in input().split()]
-print(linearSearch(a,int(input()))) 
+a=[int(x) for x in input("enter elements:").split()]
+print(linearSearch(a,int(input("enter number:")))) 
